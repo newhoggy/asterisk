b829f088df1fc856d05ab87fdde79def3f173e20
John Ky <newhoggy@gmail.com>
2019-11-18 08:05:27 +1100

    Initial project

---
+++ frontend/src/Frontend.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+module Frontend where
+
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Obelisk.Frontend
+import Obelisk.Configs
+import Obelisk.Route
+import Reflex.Dom.Core
+
+import Common.Api
+import Common.Route
+import Obelisk.Generated.Static
+
+frontend :: Frontend (R FrontendRoute)
+frontend = Frontend
+  { _frontend_head = el "title" $ text "Obelisk Minimal Example"
+  , _frontend_body = do
+      text "Welcome to Obelisk!"
+      el "p" $ text $ T.pack commonStuff
+      elAttr "img" ("src" =: static @"obelisk.jpg") blank
+      el "div" $ do
+        exampleConfig <- getConfig "common/example"        
+        case exampleConfig of
+          Nothing -> text "No config file found in config/common/example"
+          Just s -> text (T.decodeUtf8 s)
+      return ()
+  }
